0dc14dd8f49f78d3e5fa89f9ab207c3b2a0e1998
Your Name <you@example.com>
2021-06-14 14:23:04 +0530

    changed api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:2480/api",
-  // baseURL: "https://reactnotesbackend-b2tech.herokuapp.com/api",
+  // baseURL: "http://localhost:2480/api",
+  baseURL: "https://expense-tracker-backend-gana.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
