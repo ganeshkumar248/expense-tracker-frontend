6fb2da66d907177c8a6c5291bd38e45b585c4b41
Your Name <you@example.com>
2021-06-25 18:06:35 +0530

    Auth support

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost:2480/api",
+  // baseURL: "http://localhost:8080/api",
   baseURL: "https://expense-tracker-backend-gana.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
