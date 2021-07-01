dc1636b14e27e6dca115bf1d4aa4c6152c72b6c0
Your Name <you@example.com>
2021-07-01 13:03:48 +0530

    Auth support

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:2480/api/auth/";
+// const API_URL = "http://localhost:8080/api/auth/";
 const API_URL = "https://expense-tracker-backend-gana.herokuapp.com/api/auth/";
 
 const register = (username, email, password) => {
